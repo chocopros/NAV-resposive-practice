d3050ac8a1f9fe736030ca380923a0312728ded7
chocopros <jesuschock93@gmail.com>
2022-07-09 16:48:49 -0400

    Version 1.2 - START - NAV + js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+console.log("Jesus Arechider Web - Practice NAV")
+
+addEventListener('DOMContentLoaded', () => {
+    const btn_menu = document.querySelector('.btn_menu')
+    if(btn_menu){
+        btn_menu.addEventListener('click', () => {
+            const menu_items = document.querySelector('.menu_items')
+            containerList.classList.toggle('show')
+        })
+    }
+})
